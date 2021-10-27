3430cfb14d4a54d0b232df419af377c28c15cbe9
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-10-27 15:28:54 +0530

    create a homepage cmponent

--- dream/src/App.js
+++ dream/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
-import "./App.css";
+import Homepage from "./HomepageComponent";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      Welcome to dream clothing store!😍
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
---
+++ dream/src/HomepageComponent.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="directori-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="sub=title">Shop NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">JACKET</h1>
+            <span className="sub=title">Shop NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SNEAKERS</h1>
+            <span className="sub=title">Shop NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MENS</h1>
+            <span className="sub=title">Shop NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMENS</h1>
+            <span className="sub=title">Shop NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
