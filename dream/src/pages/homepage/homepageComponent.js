a3555a1ee17b5c0f17a32774e3c941f80ed124bc
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-10-28 16:28:32 +0530

    update homepage component

--- dream/src/pages/homepage/homepageComponent.js
+++ dream/src/pages/homepage/homepageComponent.js
@@ -1,37 +1,11 @@
 import React from "react";
 import "./Hompage.scss";
-import MenuItem from "../../components/menu-item/MenuItemComponent";
+import Directory from "../../components/directory/DirectoryComponent";
+
 const Homepage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <MenuItem title="HATS" />
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">JACKET</h1>
-            <span className="sub=title">Shop NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="sub=title">Shop NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="sub=title">Shop NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="sub-title">Shop NOW</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 };
