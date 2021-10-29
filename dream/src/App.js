1673f026a99ba3e45b772e9cdcafec3d8f628278
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-10-29 12:30:26 +0530

    import shop page

--- dream/src/App.js
+++ dream/src/App.js
@@ -1,21 +1,14 @@
 import React from "react";
 import Homepage from "./pages/homepage/homepageComponent";
 import { Route, Switch } from "react-router-dom";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS APGE</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/ShopComponent";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
