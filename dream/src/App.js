12145d2299fdc555355821957edde8e8539369c9
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-10-29 11:15:59 +0530

    return hatspage

--- dream/src/App.js
+++ dream/src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Homepage from "./pages/homepage/homepageComponent";
+import { Route, Switch } from "react-router-dom";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS APGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
