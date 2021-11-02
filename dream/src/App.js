36887bc9777068ec5d4eaea0e70c116a7e107764
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-11-03 02:16:59 +0530

    import header to app.js file

--- dream/src/App.js
+++ dream/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Homepage from "./pages/homepage/homepageComponent";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/ShopComponent";
+import Header from "./components/header/HeaderComponent";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
