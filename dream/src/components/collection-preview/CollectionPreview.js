efcfb821b4427182c133a2c86d44bc9a75c6b93d
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-11-02 23:58:23 +0530

    create collection preview component

--- dream/src/components/collection-preview/CollectionPreview.js
+++ dream/src/components/collection-preview/CollectionPreview.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./CollectionPreview-style.scss";
+import CollectionItemComponent from "../collection-item/CollectionItemComponent";
 
 const CollectionPreview = ({ title, items }) => {
   return (
@@ -8,8 +9,8 @@ const CollectionPreview = ({ title, items }) => {
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <div key={item.id}>{item.name}</div>
+          .map(({ id, ...otherItemPops }) => (
+            <CollectionItemComponent key={id} {...otherItemPops} />
           ))}
       </div>
     </div>
