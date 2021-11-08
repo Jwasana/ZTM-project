7bf75fc79223cf5bd50fdd3e4df798fd65e1ac12
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-11-08 06:37:23 +0530

    update sign in component

--- dream/src/components/sign-in/SignIn.js
+++ dream/src/components/sign-in/SignIn.js
@@ -1,4 +1,5 @@
 import React from "react";
+import FormInput from "../form-input/FormInput";
 
 import "./SignIn-style.scss";
 
@@ -28,22 +29,24 @@ class SignIn extends React.Component {
         <h2>I have already an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name="email"
             type="email"
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
             value={this.state.email}
+            label="email"
             required
           />
-          <label>Email</label>
-          <input
+
+          <FormInput
             name="Password"
             type="password"
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
             value={this.state.password}
+            label="password"
             required
           />
-          <label>Password</label>
+
           <input type="submit" value="Submit Form" />
         </form>
       </div>
