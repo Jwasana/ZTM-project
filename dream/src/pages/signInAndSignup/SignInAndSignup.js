63a9acc040f0789c5bfaef3a82db7cb856227d8c
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-11-11 09:28:19 +0530

    update signup and sign in page

--- dream/src/pages/signInAndSignup/SignInAndSignup.js
+++ dream/src/pages/signInAndSignup/SignInAndSignup.js
@@ -2,7 +2,13 @@ import React from "react";
 
 import "./SigninAndSignup-style.scss";
 import SignIn from "../../components/sign-in/SignIn";
+import SignUp from "../../components/sign-up/SignUp";
 
-const SIgninAndSignup = () => <SignIn />;
+const SIgninAndSignup = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+    <SignUp />
+  </div>
+);
 
 export default SIgninAndSignup;
