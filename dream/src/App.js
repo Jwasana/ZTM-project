a11f89212ebea8680adc87723c206a30852e71b0
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-11-16 15:26:30 +0530

    remove ths.satet and use redux

--- dream/src/App.js
+++ dream/src/App.js
@@ -6,33 +6,26 @@ import Header from "./components/header/HeaderComponent";
 import SIgninAndSignup from "./pages/signInAndSignup/SignInAndSignup";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import "./App.css";
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/UserAaction";
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null,
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot((snapShot) => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(),
-            },
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
           });
         });
       }
-      this.setState({ currentUser: userAuth });
+      setCurrentUser(userAuth);
     });
   }
   componentWillUnmount() {
@@ -52,5 +45,8 @@ class App extends React.Component {
     );
   }
 }
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
 
-export default App;
+export default connect(null, mapDispatchToProps)(App);
