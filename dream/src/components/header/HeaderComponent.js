66acb28fe35e5935fb84f3cab78dafe5d1a3846f
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-11-19 13:18:44 +0530

    import cart drop down into header component

--- dream/src/components/header/HeaderComponent.js
+++ dream/src/components/header/HeaderComponent.js
@@ -4,6 +4,7 @@ import { ReactComponent as Logo } from "../../assests/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
 import CartIcon from "../cart-icon/CartIcon";
+import CartDropdown from "../cart-dropdown/CartDropdown";
 
 import "./Header-style.scss";
 
@@ -33,6 +34,7 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 const mapStateToProps = (state) => ({
