c363da39f674471bd8cbc3abd15f79bcb8456631
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-11-21 10:25:06 +0530

    include cart icon toggle option

--- dream/src/components/header/HeaderComponent.js
+++ dream/src/components/header/HeaderComponent.js
@@ -8,7 +8,7 @@ import CartDropdown from "../cart-dropdown/CartDropdown";
 
 import "./Header-style.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <div className="logo-container">
       <Link to="/">
@@ -34,11 +34,12 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
-    <CartDropdown />
+    {hidden ? null : <CartDropdown />}
   </div>
 );
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
