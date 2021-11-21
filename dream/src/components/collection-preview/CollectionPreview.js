b28e4433457f79bb0466187e2aeb8485d52ceeb4
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-11-21 15:25:47 +0530

    update collection preview

--- dream/src/components/collection-preview/CollectionPreview.js
+++ dream/src/components/collection-preview/CollectionPreview.js
@@ -9,8 +9,8 @@ const CollectionPreview = ({ title, items }) => {
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
-          .map(({ id, ...otherItemPops }) => (
-            <CollectionItemComponent key={id} {...otherItemPops} />
+          .map((item) => (
+            <CollectionItemComponent key={item.id} item={item} />
           ))}
       </div>
     </div>
