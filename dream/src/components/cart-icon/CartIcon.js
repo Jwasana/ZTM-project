0d220af509767e22a046a334825ea523f5fa9cf0
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-11-21 10:24:24 +0530

    update cart icon

--- dream/src/components/cart-icon/CartIcon.js
+++ dream/src/components/cart-icon/CartIcon.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { ReactComponent as ShoppingIcon } from "../../assests/shopping-bag.svg";
+import { connect } from "react-redux";
+import { CartToggeleHidden } from "../../redux/cart/CartActions";
 
 import "./CartIcon-style.scss";
 
-const CartIcon = () => (
-  <div className="cart-icon">
+const CartIcon = ({ CartToggeleHidden }) => (
+  <div className="cart-icon" onClick={CartToggeleHidden}>
     <ShoppingIcon className="shoping-icon" />
     <span className="item-count">0</span>
   </div>
 );
-export default CartIcon;
+const mapDispachToPrps = (dispatch) => ({
+  CartToggeleHidden: () => dispatch(CartToggeleHidden()),
+});
+export default connect(null, mapDispachToPrps)(CartIcon);
