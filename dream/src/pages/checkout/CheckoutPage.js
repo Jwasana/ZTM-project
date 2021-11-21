36511705a796dbeca0d0f626bb91642593d02b25
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-11-22 02:56:36 +0530

    gg

--- dream/src/pages/checkout/CheckoutPage.js
+++ dream/src/pages/checkout/CheckoutPage.js
@@ -9,6 +9,7 @@ import {
   selectCartTotal,
 } from "../../redux/cart/CartSelectors";
 import CheckoutItem from "../../components/checkout-item/CheckoutItem";
+import StripeCheckoutButton from "../../components/stripe-button/StripeButton";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -32,9 +33,13 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map((cartItem) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className="total">
-      <span>TOTAL: ${total}</span>
+    <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+      *Please use following credit card to payment*
+      <br />
+      4242 4242 4242 4242 ex-01/20 CVV- 123
     </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
