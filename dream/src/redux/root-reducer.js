efd134b806bfa8473008aee08aad7d91321956ac
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-11-21 23:05:35 +0530

    145

--- dream/src/redux/root-reducer.js
+++ dream/src/redux/root-reducer.js
@@ -3,6 +3,7 @@ import { persistReducer } from "redux-persist";
 import UserReducer from "./user/UserReducer";
 import CartReducer from "./cart/CartReducer";
 import storage from "redux-persist/lib/storage";
+import directoryReducer from "./directory/DirectoryReducer";
 
 const persistConfig = {
   key: "root",
@@ -12,6 +13,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: UserReducer,
   cart: CartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
