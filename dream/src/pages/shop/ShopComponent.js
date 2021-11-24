5fadba35f3f3b4eb6e63c73764f46a44e7be32b5
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-11-24 12:50:45 +0530

    remove console log

--- dream/src/pages/shop/ShopComponent.js
+++ dream/src/pages/shop/ShopComponent.js
@@ -5,7 +5,6 @@ import CollectionPage from "../collection/Collection";
 import CollectionOveriew from "../../components/collections-overview/CollectionOveriew";
 
 const ShopPage = ({ match }) => {
-  console.log(match);
   return (
     <div className="shop-page">
       <Route exact path={`${match.path}`} component={CollectionOveriew} />
